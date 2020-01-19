8e2bea2239a686a8c4a7f9ad6b32f1ecbb184fc1
Arturo Bravo Rovirosa <arturo123leyenda@gmail.com>
2020-01-19 12:39:14 -0800

    react router and redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
+import { logoutRequest } from '../actions';
 
-const Header = () => (
-  <header className="header">
-    <img className="header__img" src={logo} alt="Platzi Video" />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = (props) => {
+  const { user } = props;
+
+  const hasUser = Object.keys(user).length > 0; //check if user exist
+
+  const handleLogOut = () => {
+    props.logoutRequest({});
+  };
+
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt='Platzi Video' />
+          }
+
+          <p>Perfil</p>
+        </div>
+        <ul>
+
+          {hasUser ? (
+            <li><a href='/'>{user.name}</a></li>
+          ) : null
+          }
+
+          {hasUser ?
+            <li><a href='#logout' onClick={handleLogOut}> Cerrar Sesión </a></li> : (
+              <li><Link to='/login'>Iniciar Sesión</Link></li>
+            )}
+
+        </ul>
       </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
---
+++ src/containers/Home.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import '../assets/styles/App.scss';
+
+const Home = ({ mylist, trends, originals }) => {
+  return (
+    <Fragment>
+      <Search />
+      {mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {mylist.map(item => <CarouselItem key={item.id} {...item} isList />)}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Tendencias'>
+        <Carousel>
+          {trends.map(item => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {originals.map(item => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+    </Fragment>
+  );
+};
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home); //mapStateToProps los datos que necesitas de redux y lo otro son las acciones
+
